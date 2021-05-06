dff608eb637c02cfac6e9e02cee4bfed76d9e936
Anzala Ahmad Sauqi <anzalaahmad14@gmail.com>
2021-05-06 20:48:11 +0700

    UTS

---
+++ UTS/UTS_PRPL_NO4/UTS_PRPL/koneksi.php
@@ -0,0 +1,6 @@
+<?php
+$host           = "localhost";
+$user           = "root";
+$password       = "";
+$database       = "uts_prpl";
+$connect        = mysqli_connect($host, $user, $password, $database);
---
+++ UTS/UTS_PRPL_NO4/uts_prpl.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 06 Bulan Mei 2021 pada 15.10
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.2.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uts_prpl`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`) VALUES
+(5, 'anzalahehat@gmail.com', 'anzalaaas123'),
+(6, 'anzalahehat@gmail.com', 'anzalaaas123'),
+(7, 'anzalahehat@gmail.com', 'anzalaas123'),
+(8, 'anzalahehat@gmail.com', 'asdasdasdas');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
